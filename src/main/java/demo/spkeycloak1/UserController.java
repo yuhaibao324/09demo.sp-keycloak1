de396be186379759261908e711717a9deea9bfa0
正义之剑 <yuhaibao324@126.com>
2019-01-27 21:08:55 +0800

    会员信息显示， by keycloak~~

--- src/main/java/demo/spkeycloak1/UserController.java
+++ src/main/java/demo/spkeycloak1/UserController.java
@@ -4,6 +4,8 @@
 package demo.spkeycloak1;
 
 import java.security.Principal;
+import java.util.Map;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
@@ -19,10 +21,13 @@ public class UserController {
     final static Logger LOG = LoggerFactory.getLogger(UserController.class);
 
     @RequestMapping("/user")
-    public String index(Principal p) {
+    public String index(Principal p, Map<String,Object> map) {
         System.out.println("------"+p.getName()+"--------->");
         LOG.debug("p:{}", p != null ? p.getName() : "null");
         System.out.println("--------------->");
+
+        map.put("username1", p.getName());
+        map.put("hello", p.getName());
         return "user";
     }
 
